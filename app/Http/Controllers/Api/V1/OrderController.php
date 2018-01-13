996f785e9e5ea1ac242d92f26f9aa8ff2737fd66
Eries Trisnadi <me@eries.id>
2018-01-13 21:53:57 +0700

    multiple order, bugfix filtering forgot to use carbon

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Api\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Collection;
 use App\Http\Controllers\Controller;
 use App\Product;
 use App\Order;
+use Carbon\Carbon;
 use App\OrderDetail;
 use Illuminate\Support\Facades\Auth;
 
@@ -43,14 +45,14 @@ class OrderController extends Controller
             }
 
             if(is_object($params->date_range) && ($params->date_range->from && $params->date_range->to)){
-                $customer = $customer->whereBetween('created_at', [
+                $order = $order->whereBetween('created_at', [
                     Carbon::createFromFormat('m/d/Y', $params->date_range->from)->format('Y-m-d')." 00:00:00",
                     Carbon::createFromFormat('m/d/Y', $params->date_range->to)->format('Y-m-d')." 23:59:59"
                 ]);
             }
         }
 
-        if($this->content['data'] = $order->get()){
+        if($this->content['data'] = $order->orderBy('created_at', 'desc')->get()){
           $this->content['status'] = 200;
           return response()->json($this->content, $this->content['status'], [], JSON_NUMERIC_CHECK);
         }
@@ -79,28 +81,52 @@ class OrderController extends Controller
 
     public function add(Request $request)
     {
-        $product = Product::find($request->input('order_detail.product.id'));
-        if ($product->stock >= $request->input('order_detail.amount')){
-          $product->update([
-            'stock' => (int) $product->stock - $request->input('order_detail.amount')
-          ]);
-          
-          $order = Order::create([
-            'customer_id' => $request->input('customer.id'),
-            'company_id' => Auth::user()->company->id
-          ]);
-  
-          $order_detail = OrderDetail::create([
-            'order_id' => $order->id,
-            'product_id' => $product->id,
-            'amount' => $request->input('order_detail.amount')
-          ]);
+        if (is_array($request->all())) {
+            collect($request->all())->map(function($data){
+                $std = (object) json_decode(json_encode($data));
+                $product = Product::find($std->order_detail->product->id);
+                if ($product->stock >= $std->order_detail->amount){
+                    $product->update([
+                        'stock' => (int) $product->stock - $std->order_detail->amount
+                    ]);
+                    
+                    $order = Order::create([
+                        'customer_id' => $std->customer->id,
+                        'company_id' => Auth::user()->company->id
+                    ]);
+            
+                    $order_detail = OrderDetail::create([
+                        'order_id' => $order->id,
+                        'product_id' => $product->id,
+                        'amount' => $std->order_detail->amount
+                    ]);
+                }
+            });
+        } else {
+            $product = Product::find($request->input('order_detail.product.id'));
+            if ($product->stock >= $request->input('order_detail.amount')){
+                $product->update([
+                    'stock' => (int) $product->stock - $request->input('order_detail.amount')
+                ]);
+                
+                $order = Order::create([
+                    'customer_id' => $request->input('customer.id'),
+                    'company_id' => Auth::user()->company->id
+                ]);
+        
+                $order_detail = OrderDetail::create([
+                    'order_id' => $order->id,
+                    'product_id' => $product->id,
+                    'amount' => $request->input('order_detail.amount')
+                ]);
+            }
+        }
 
-          if($this->content['data'] = Order::with(['order_detail.product', 'customer'])
-                                            ->where('company_id', Auth::user()->company->id)->get()){
+        if($this->content['data'] = Order::with(['order_detail.product', 'customer'])
+                                           ->where('company_id', Auth::user()->company->id)
+                                           ->orderBy('created_at', 'desc')->get()){
             $this->content['status'] = 200;
             return response()->json($this->content, $this->content['status'], [], JSON_NUMERIC_CHECK);
-          }
         }
 
         $this->content['error'] = "Server Error";
