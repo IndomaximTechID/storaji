fd5235ae8dd1040f689a2f60913e6180f0c8608a
Eries Trisnadi <me@eries.id>
2018-02-11 21:08:27 +0700

    add graph of the week

--- app/Http/Controllers/Api/V1/StatsController.php
+++ app/Http/Controllers/Api/V1/StatsController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
 use DB;
+use App\Order;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class StatsController extends Controller
 {
@@ -15,20 +17,49 @@ class StatsController extends Controller
     }
 
     public function stats(Request $request){
+        $weeks = [0,1,2,3,4,5,6];
+        $dt = Carbon::now();
+        $lt = Carbon::now()->subWeek();
         $products = DB::table('products')->where('company_id', Auth::user()->company->id)->whereNull('deleted_at')->count();
         $customers = DB::table('customers')->where('company_id', Auth::user()->company->id)->whereNull('deleted_at')->count();
         $orders = DB::table('orders')->where('company_id', Auth::user()->company->id)->whereNull('deleted_at')->count();
 
-        $product_amounts = DB::table('order_details')
+        $stat_amounts = DB::table('order_details')
                  ->join('products', 'order_details.product_id', '=', 'products.id')
                  ->join('orders', 'order_details.order_id', '=', 'orders.id')
                  ->where([
                    'products.company_id' => Auth::user()->company->id,
                    'orders.deleted_at' => null
                   ])
-                 ->select(DB::raw('product_id, count(product_id) as unit, amount'))
-                 ->groupBy('product_id')
-                 ->get();
+                  ->select(DB::raw('product_id, count(product_id) as unit, amount'))
+                  ->groupBy('product_id')
+                  ->get();
+        
+        $_pre = Order::with(['order_detail.product', 'customer'])
+                ->where('company_id', Auth::user()->company->id);
+        
+        $currentWeekRevenue = $_pre
+          ->whereBetween('created_at', [
+            $dt->startOfWeek()->toDateTimeString(),
+            $dt->endOfWeek()->toDateTimeString()
+          ])
+          ->get();
+
+        $lastWeekRevenue = $_pre
+          ->whereBetween('created_at', [
+            $lt->startOfWeek()->toDateTimeString(),
+            $lt->endOfWeek()->toDateTimeString()
+          ])
+          ->get();
+
+        $mapToZero = function($v) {
+          return 0;
+        };
+        
+        $graph = (object) [
+          'current' => (array) array_map($mapToZero, $weeks),
+          'last' => (array) array_map($mapToZero, $weeks)
+        ];
         
         $stats = (object) [
           'cost' => 0,
@@ -36,7 +67,29 @@ class StatsController extends Controller
           'profit' => 0
         ];
 
-        foreach ($product_amounts as $index) {
+        foreach ($weeks as $index) {
+          foreach ($currentWeekRevenue as $current) {
+            $curdate = $dt->startOfWeek()->addDays($index + 1);
+            $dbdate = Carbon::createFromFormat('Y-m-d H:i:s', $current->created_at)->format('Y-m-d');
+
+            if($curdate->format('Y-m-d') === $dbdate) {
+              $revenue = $current->order_detail->product->selling_price * $current->order_detail->amount;
+              $graph->current[$curdate->dayOfWeek] += $revenue;
+            }
+          }
+          
+          foreach ($lastWeekRevenue as $last) {
+            $curdate = $lt->startOfWeek()->addDays($index + 1);
+            $dbdate = Carbon::createFromFormat('Y-m-d H:i:s', $last->created_at)->format('Y-m-d');
+
+            if($curdate->format('Y-m-d') === $dbdate) {
+              $revenue = $last->order_detail->product->selling_price * $last->order_detail->amount;
+              $graph->last[$curdate->dayOfWeek] += $revenue;
+            }
+          }
+        }
+
+        foreach ($stat_amounts as $index) {
           $pre = DB::table('products')
                      ->where('company_id', Auth::user()->company->id)
                      ->find($index->product_id);
@@ -52,6 +105,7 @@ class StatsController extends Controller
           'customers' => $customers,
           'orders' => $orders,
           'stats' => $stats,
+          'graph' => $graph
         ]){
           $this->content['status'] = 200;
           return response()->json($this->content, $this->content['status'], [], JSON_NUMERIC_CHECK);
